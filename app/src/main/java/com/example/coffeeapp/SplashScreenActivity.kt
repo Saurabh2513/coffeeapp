d31015dfa8220f5c2d68200e59d392e88be9db03
sagar <sagaryadav12369@gmail.com>
2025-06-16 15:02:20 +0530

    splashscreen

---
+++ app/src/main/java/com/example/coffeeapp/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.coffeeapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.coffeeapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+}
